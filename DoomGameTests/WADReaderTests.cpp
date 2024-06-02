23938e954de0677cc3ef5824541e5afbf20515e0
Luke <lukedmcalpine@gmail.com>
2024-06-01 17:03:37 -0700

    Update comments in WADReaderTests.cpp to be more accurate

--- DoomGameTests/WADReaderTests.cpp
+++ DoomGameTests/WADReaderTests.cpp
@@ -42,8 +42,24 @@ static TEST_F(WADReaderTests, HandleTotalLumps)
 static TEST_F(WADReaderTests, HandleRead2Bytes)
 {
 	std::vector<std::byte> buffer = { std::byte{0x01}, std::byte{0x02}, std::byte{0x03}, std::byte{0x04}, std::byte{0x05}, std::byte{0x06} };
-	// Expecting 513 because 0x0201 in little-endian is 0x01 at the LSB and 0x02 at the MSB and 513 is the decimal representation
+
+	// The bytes are stored in order in memory as 01 02 03 04 05 06
+	// Because the elements of the array are single bytes, there is no endianness applied to the individual bytes
+	// When interpreting the bytes in memory as two byte integers on a little endian machine, we will get: 
+	// 0x0201 from bytes 01 and 02
+	// 0x0403 from bytes 03 and 04,
+	// 0x0605 from bytes 05 and 06
+	// For each pair of bytes, the byte at the lower address is the LSB, and the byte at the higher address is the MSB
+	// - The byte 0x01 (lower address) is the LSB, and the byte 0x02 (higher address) is the MSB, forming the two-byte integer value 0x0201.
+	// - The byte 0x03 (lower address) is the LSB, and the byte 0x04 (higher address) is the MSB, forming the two-byte integer value 0x0403.
+	// - The byte 0x05 (lower address) is the LSB, and the byte 0x06 (higher address) is the MSB, forming the two-byte integer value 0x0605.
+	// In decimal:
+	// - 0x0201 is 513
+	// - 0x0403 is 1027
+	// - 0x0605 is 1541
 	ASSERT_EQ(wadReader.read2Bytes(buffer, 0), 513);
+	ASSERT_EQ(wadReader.read2Bytes(buffer, 2), 1027);
+	ASSERT_EQ(wadReader.read2Bytes(buffer, 4), 1541);
 }
 
 static TEST_F(WADReaderTests, HandleReadOutOfBounds2Bytes)
@@ -74,7 +90,16 @@ static TEST_F(WADReaderTests, HandleOutOfBoundsEqual2Bytes)
 static TEST_F(WADReaderTests, HandleRead4Bytes)
 {
 	std::vector<std::byte> buffer = { std::byte{0x01}, std::byte{0x02}, std::byte{0x03}, std::byte{0x04}, std::byte{0x05}, std::byte{0x06} };
-	// Expecting 67305985 because 0x04030201 in little-endian is the decimal representation
+
+	// The bytes are stored in order in memory as 01 02 03 04 05 06
+	// Because the elements of the array are single bytes, there is no endianness applied to the individual bytes
+	// When interpreting the bytes in memory as four byte integers on a little endian machine, we will get: 
+	// 0x04030201 from bytes 04, 03, 01 and 02
+	// For each pair of bytes, the byte at the lower address is the LSB, and the byte at the higher address is the MSB
+	// - The byte 0x01 (lower address) is the LSB, and the byte 0x04 (higher address) is the MSB, forming the four-byte integer value 0x04030201.
+	// In decimal:
+	// - 0x04030201 is 67305985
+
 	ASSERT_EQ(wadReader.read4Bytes(buffer, 0), 67305985);
 }
 
